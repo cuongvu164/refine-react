4be5d119e289673da7a8f7ba18805211192df45d
cuongvu164 <cuongmk288@gmail.com>
2022-02-14 22:35:47 +0700

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { Refine } from "@pankod/refine-core";
+import { notificationProvider } from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router";
+import "@pankod/refine-antd/dist/styles.min.css";
+import dataProvider from "@pankod/refine-simple-rest";
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+
+function App() {
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      notificationProvider={notificationProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      resources={[
+        {
+          name: "posts",
+          list: PostList,
+          create: PostCreate,
+          edit: PostEdit,
+          show: PostShow,
+        },
+      ]}
+      Title={Title}
+      Header={Header}
+      Sider={Sider}
+      Footer={Footer}
+      Layout={Layout}
+      OffLayoutArea={OffLayoutArea}
+    />
+  );
+}
+
+export default App;
